ced6b91af652a3ef8d9b9efc51a85b95b4f3bbff
ts <2629774907@qq.com>
2025-04-28 14:41:46 +0800

    04

--- 图像拼接.py
+++ 图像拼接.py
@@ -7,6 +7,10 @@ img2 = cv2.imread('photo/pinjie2.png')
 # 设置同一个尺寸
 img1 = cv2.resize(img1, (640, 480))
 img2 = cv2.resize(img2, (640, 480))
+cv2.imshow('img1', img1)
+cv2.imshow('img2', img2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 # 灰度变换
 gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
@@ -27,7 +31,30 @@ if len(good) > 4:
     src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
     dst_pts = np.float32([kp2[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
 
-# 计算单适应性矩阵
-H, _ = cv2.findHomography(src_pts, dst_pts, cv2.RANSSAC, 5)
+    # 计算单适应性矩阵
+    H, _ = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5)
 
+else:
+    print('erorr')
+    exit(0)
+# 获取原始图像的高和宽
+h1, w1 = img1.shape[:2]
+h2, w2 = img2.shape[:2]
+img1_pts = np.float32([[0, 0], [0, h1], [w1, h1], [w1, 0]]).reshape(-1, 1, 2)
+img2_pts = np.float32([[0, 0], [0, h2], [w2, h2], [w2, 0]]).reshape(-1, 1, 2)
+
+img1_transform = cv2.perspectiveTransform(img1_pts, H)
+
+result_pts = np.concatenate((img2_pts, img1_transform), axis=0)
+[x_min, y_min] = np.int32(result_pts.min(axis=0).ravel() - 1)
+[x_max, y_max] = np.int32(result_pts.max(axis=0).ravel() + 1)
+
+H_transform = np.array([[1, 0, -x_min], [0, 1, -y_min], [0, 0, 1]])
+result_img = cv2.warpPerspective(img1, H_transform.dot(H), (x_max-x_min, y_max-y_min))
+
+result_img[-y_min: -y_min+h2, -x_min: -x_min+w2] = img2
+
+cv2.imshow('result', result_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
